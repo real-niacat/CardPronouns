fbfc57ce9ee23430886fb5469ddf46d393b3da0f
rosie <103908705+rosie-qwq@users.noreply.github.com>
2025-09-27 07:34:55 -0300

    code there was not necessary im pretty sure?? it works fine

--- main.lua
+++ main.lua
@@ -4,20 +4,6 @@ CardPronouns.badge_types = {}
 CardPronouns.classifications = {}
 CardPronouns.PlayingCardOverrides = {}
 
-local function load_pronouns()
-    for key, cent in pairs(G.P_CENTERS) do
-        G.P_CENTERS[key].pronouns = G.P_CENTERS[key].pronouns or CardPronouns.badge_by_string(key).key
-    end
-end
-
-local smodsinject = SMODS.injectItems
-
-function SMODS.injectItems(...)
-    smodsinject(...)
-
-    load_pronouns()
-end
-
 assert(SMODS.load_file("util.lua"))()
 assert(SMODS.load_file("api.lua"))()
 assert(SMODS.load_file("badge.lua"))()
